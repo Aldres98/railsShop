2aa152434681e5abd7a82aa4e8ae4bf5d8190738
Aldres98 <aldres98@gmail.com>
2017-05-18 13:42:19 +0300

    add show method to products + user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
+devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
+  root to: "products#index"
   
 end
